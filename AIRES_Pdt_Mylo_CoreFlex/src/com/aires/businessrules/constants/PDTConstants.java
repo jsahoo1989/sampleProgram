77f3781ede3eb9f7c0ae7242c4ae494df9bc8d67
Sarika Pant <spant@aires.com>
2022-07-05 02:29:06 -0400

    PDT|Sprint-29|Changed Education Assistant to Education Assistance

--- AIRES_Pdt_Mylo_CoreFlex/src/com/aires/businessrules/constants/PDTConstants.java
+++ AIRES_Pdt_Mylo_CoreFlex/src/com/aires/businessrules/constants/PDTConstants.java
@@ -350,7 +350,7 @@ public class PDTConstants {
 	public static final String DEPARTURE_SERVICES = "Departure Services";
 	public static final String FURNITURE_RENTAL = "Furniture Rental";
 	public static final String REIMBURSEMENT_OF_MEMEBERSHIP_DUES = "Reimbursement of Membership Dues";
-	public static final String SCHOOL_SEARCH = "Education Assistant";
+	public static final String EDUCATION_ASSISTANCE = "Education Assistance";
 	public static final String SETTLING_IN_SERVICES = "Settling In Services";
 	public static final String TRANSITION_ASSISTANCE_PROGRAM = "Transition Assistance Program";
 	public static final String TUTION_AND_EDUCATION = "Tuition/Education Payments";

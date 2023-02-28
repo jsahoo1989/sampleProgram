265a039b4436a0f01c946fe0fc74ded070858cf6
Vinod Mallah <Vmallah@aires.com>
2023-02-28 02:37:29 -0500

    Comitting Test Proc

--- AIRES_Pdt_Mylo_CoreFlex/src/com/aires/utilities/TesProc.java
+++ AIRES_Pdt_Mylo_CoreFlex/src/com/aires/utilities/TesProc.java
@@ -1,16 +1,47 @@
 package com.aires.utilities;
 
-import java.io.FileInputStream;
-import com.aires.businessrules.DbFunctions;
+import org.eclipse.swt.internal.win32.OS;
+
+import com.sun.jna.Native;
+import com.sun.jna.Pointer;
+import com.sun.jna.win32.StdCallLibrary;
 
 public class TesProc {
-	public static String  path;
-	public static FileInputStream fis = null;
 
-	
-	 public static void main(String[] args)  {
-		 Log.info("Start");
-		 DbFunctions.deletePolicyByPolicyId(14724);
-		 Log.info("Stop");		
-	 }
+	static interface User32 extends StdCallLibrary {
+		@SuppressWarnings("deprecation")
+		User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class);
+
+		interface WNDENUMPROC extends StdCallCallback {
+			boolean callback(Pointer hWnd, Pointer arg);
+		}
+
+		boolean EnumWindows(WNDENUMPROC lpEnumFunc, Pointer userData);
+
+		int GetWindowTextA(Pointer hWnd, byte[] lpString, int nMaxCount);
+
+		Pointer GetWindow(Pointer hWnd, int uCmd);
+	}
+
+	public static void setIRISForegroundWindow() {
+		final User32 user32 = User32.INSTANCE;
+
+		user32.EnumWindows(new User32.WNDENUMPROC() {
+
+			@Override
+			public boolean callback(Pointer hWnd, Pointer arg) {
+				byte[] windowText = new byte[512];
+				user32.GetWindowTextA(hWnd, windowText, 512);
+				String wText = Native.toString(windowText).trim();
+				if (!wText.isEmpty() && wText.contains("IRIS Login")) {				
+					OS.SetForegroundWindow(hWnd.hashCode());
+				}
+				return true;				
+			}
+		}, null);
+	}
+
+	public static void main(String[] args) {
+		setIRISForegroundWindow();
+	}
 }
